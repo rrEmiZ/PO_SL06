1219ac6ef5b1ac0a90cb87340d4182c6d7f66b4c
Gabriel <drozd.gj@gmail.com>
2021-04-29 19:42:56 +0200

    Lab5 - zadanie 1 (a, b, c)

---
+++ LAB5/Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exceptions
+{
+    class Program
+    {
+        public static int WordLength(string word)
+        {
+            var wordLength = word.Length;
+            return wordLength;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                WordLength(null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message = {0}", ex.Message);
+                Console.WriteLine("Source = {0}", ex.Source);
+                Console.WriteLine("StackTrace:\n{0}", ex.StackTrace);
+                Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
