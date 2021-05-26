58503520be987cd4c164c9d037537edde14a8a72
Jakub Jońca <jakubjonca@outlook.com>
2021-05-26 15:57:56 +0200

    EF EDM

---
+++ ConsoleApp1/ConsoleApp1/Products/Dtos/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Products.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Products/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Products
+{
+    public class ProductRepository
+    {
+        private readonly BikeStoresEntities _db;
+
+        public ProductRepository(BikeStoresEntities db)
+        {
+            _db = db;
+        }
+
+        public List<ProductDto> GetProducts()
+        {
+
+            return _db.products.Select(x => new ProductDto()
+            {
+                BrandName = x.brand.brand_name,
+                CategoryName = x.category.category_name,
+                Id = x.product_id,
+                Name = x.product_name
+            }).ToList();
+
+        }
+
+
+    }
+}
