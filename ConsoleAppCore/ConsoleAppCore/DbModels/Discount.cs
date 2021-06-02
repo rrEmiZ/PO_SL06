5b6e5a75cfe8fe7e4fbada43663e53dabc31f237
Jakub Jońca <jakubjonca@outlook.com>
2021-06-02 16:11:25 +0200

    CodeFirst

---
+++ ConsoleAppCore/ConsoleAppCore/DbModels/Discount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppCore.DbModels
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        public string Code { get; set; }
+    }
+}
---
+++ ConsoleAppCore/ConsoleAppCore/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using ConsoleAppCore.DbModels;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using(var db = new BikeStoresContext())
+            {
+                //var items = db.Brands.ToList();
+
+                //var first = items.FirstOrDefault();
+
+                //first.BrandName += "Test";
+
+                //db.Update(first);
+
+                //db.SaveChanges();
+
+                //db.Discounts.Add(new Discount() { Value = 2, Code = "TEST"});
+
+                //db.SaveChanges();
+
+
+                using(var trans = db.Database.BeginTransaction())
+                {
+                    var brand = new Brand()
+                    {
+                        BrandName = "Testtt"
+                    };
+
+                    db.Brands.Add(brand);
+                    db.SaveChanges();
+
+
+                    var prod = new Product()
+                    {
+                        BrandId = brand.BrandId,
+                        ProductName = "tet",
+                        CategoryId = 1,
+                        ListPrice = 4,
+                        ModelYear = 2000
+                    };
+
+                    db.Products.Add(prod);
+
+                    db.SaveChanges();
+
+
+                    trans.Commit();
+                }
+
+
+
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
